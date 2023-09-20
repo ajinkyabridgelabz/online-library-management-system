5e2527979a84726c9ed89a93a21a8b1059eddf88
ajinkyabridgelabz <ajinkya.kambe@bridgelabz.com>
2023-09-20 13:42:39 +0530

    Spring starter code

---
+++ src/main/java/com/bridgelabz/onlinelibrarymanagementsystem/OnlineLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.onlinelibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/onlinelibrarymanagementsystem/OnlineLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.onlinelibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
